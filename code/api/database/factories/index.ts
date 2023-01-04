faf739f91abb34d837370a421e30ae143a8e3586
Luis Fernando <luisfernandinho094@gmail.com>
2023-01-03 22:54:32 -0300

    feat: creating factories for models

--- code/api/database/factories/index.ts
+++ code/api/database/factories/index.ts
@@ -1,1 +1,42 @@
-// import Factory from '@ioc:Adonis/Lucid/Factory'
+import Factory from '@ioc:Adonis/Lucid/Factory'
+
+import Course from 'App/Models/Course'
+import CourseSection from 'App/Models/CourseSection'
+import User from 'App/Models/User'
+import Video from 'App/Models/Video'
+
+export const UserFactory = Factory.define(User, ({ faker }) => {
+  return {
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  }
+}).build()
+
+export const CourseFactory = Factory.define(Course, ({ faker }) => {
+  return {
+    name: faker.lorem.words(),
+    description: faker.lorem.sentence(),
+    price: Math.floor(Math.random() * 30),
+  }
+})
+  .relation('owner', () => UserFactory)
+  .relation('sections', () => CourseSectionFactory)
+  .build()
+
+export const CourseSectionFactory = Factory.define(CourseSection, ({ faker }) => {
+  return {
+    name: faker.lorem.words(),
+  }
+})
+  .relation('course', () => CourseFactory)
+  .build()
+
+export const VideoFactory = Factory.define(Video, ({ faker }) => {
+  return {
+    name: faker.lorem.words(),
+    description: faker.lorem.paragraph(),
+  }
+})
+  .relation('section', () => CourseSectionFactory)
+  .build()
