e759c27c4de5b4c7b7aa8fbb85b96bd2e4e58b87
Luis Fernando <luisfernandinho094@gmail.com>
2023-01-20 12:02:27 -0300

    feat: generating new qualities

--- code/video/src/services/ffmpeg.ts
+++ code/video/src/services/ffmpeg.ts
@@ -24,18 +24,62 @@ export function extractImages(
     ffmpeg()
       .input(file)
       .addInputOptions(["-ss", startTime])
-      .addOutputOptions([
-        "-t",
-        duration,
-        "-vf",
-        "fps=10/1,scale=540:-1",
-      ])
+      .addOutputOptions(["-t", duration, "-vf", "fps=10/1,scale=540:-1"])
       .output(output)
       .on("end", () => {
         resolve();
       })
       .on("error", (err) => {
         return reject(err);
+      })
+      .run();
+  });
+}
+
+export function resizeVideo(file: string, outDir: string) {
+  return new Promise<void>((resolve, reject) => {
+    ffmpeg()
+      .input(file)
+      .addInputOptions([
+        "-hwaccel",
+        "vaapi",
+        "-hwaccel_output_format",
+        "vaapi",
+        "-hwaccel_device",
+        "/dev/dri/renderD128",
+      ])
+      .output(outDir + "/output_360.mp4")
+      .addOutputOptions([
+        "-vf",
+        "hwupload,fps=30/1,scale_vaapi=-1:360",
+        "-c:v",
+        "h264_vaapi",
+      ])
+      .output(outDir + "/output_480.mp4")
+      .addOutputOptions([
+        "-vf",
+        "hwupload,fps=30/1,scale_vaapi=-1:480",
+        "-c:v",
+        "h264_vaapi",
+      ])
+      .output(outDir + "/output_720.mp4")
+      .addOutputOptions([
+        "-vf",
+        "hwupload,fps=30/1,scale_vaapi=-1:720",
+        "-c:v",
+        "h264_vaapi",
+      ])
+      .on("start", function (commandLine) {
+        console.log("Spawned Ffmpeg with command: " + commandLine);
+      })
+      .on("end", () => {
+        resolve();
+      })
+      .on("error", (err) => {
+        return reject(err);
+      })
+      .on("progress", function (progress) {
+        console.log("Processing: " + progress.percent + "% done");
       })
       .run();
   });
