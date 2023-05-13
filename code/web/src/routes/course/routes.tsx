1bc50fc2e3173e3e75841f471f0d14edfc3251ee
Alisson Lívio <livilia74629@gmail.com>
2023-05-13 16:37:29 -0300

    new routes

--- code/web/src/routes/course/routes.tsx
+++ code/web/src/routes/course/routes.tsx
@@ -1,10 +1,11 @@
 import { RouteObject } from "react-router-dom";
 
 import Course from "@routes/course";
-import Settings from "@routes/course/Settings";
-import SettingsVideos from "@routes/course/SettingsVideos";
+import Videos from "@routes/course/Videos";
 import UploadVideo from "@routes/course/UploadVideo";
 import Create from "@routes/course/Create";
+import SettingsCourse from "@routes/course/SettingsCourse";
+import AnyCourse from "./AnyCourse";
 
 const routes: RouteObject[] = [
   {
@@ -12,12 +13,8 @@ const routes: RouteObject[] = [
     element: <Course />,
   },
   {
-    path: "/course/:id/settings",
-    element: <Settings />,
-  },
-  {
     path: "/course/:id/settings/videos",
-    element: <SettingsVideos />,
+    element: <Videos />,
   },
   {
     path: "/course/:id/settings/upload",
@@ -27,6 +24,15 @@ const routes: RouteObject[] = [
     path: "/course/create",
     element: <Create />,
   },
+  {
+    path:"course/:id/settings/settingscourse",
+    element: <SettingsCourse/>,
+  },
+  {
+    path:"course/anycourse",
+    element: <AnyCourse />,
+  }
+  
 ];
 
 export default routes;
