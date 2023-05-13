7387653b31cc63866b6e1fb039f604005155a2ad
Alisson Lívio <livilia74629@gmail.com>
2023-05-13 16:37:08 -0300

    upload video

--- code/web/src/routes/course/UploadVideo.tsx
+++ code/web/src/routes/course/UploadVideo.tsx
@@ -1,9 +1,51 @@
+import React from 'react';
+import { InboxOutlined } from '@ant-design/icons';
+import type { UploadProps } from 'antd';
+import { message, Upload } from 'antd';
 import LayoutSideBar from "@components/LayoutSideBar";
+
+
+const { Dragger } = Upload;
+
+/*const props: UploadProps = {
+  name: 'file',
+  multiple: true,
+  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+  onChange(info) {
+    const { status } = info.file;
+    if (status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+  onDrop(e) {
+    console.log('Dropped files', e.dataTransfer.files);
+  },
+};
+*/
+
+
 
 export default function UploadVideo() {
   return (
     <LayoutSideBar>
-      <h1>Upload de video</h1>
+      <h1 className="font-sans font-bold text-2xl ml-14 mt-10">Upload Videos</h1>
+      <div style={{ width: '395px', height: '172px', marginLeft: ' 500px', marginTop:'172px' }}>
+        <Dragger >
+          <p className="ant-upload-drag-icon">
+            <InboxOutlined />
+          </p>
+          <p className="ant-upload-text">Click or drag file to this area to upload</p>
+          <p className="ant-upload-hint">
+            Support for a single or bulk upload. Strictly prohibited from uploading company data or other
+            banned files.
+          </p>
+        </Dragger>
+      </div>
     </LayoutSideBar>
   );
 }
